f46f3bab6fb7b9aeb0ff8bdb061185bf2ec00e35
leandermaben <leander.maben@gmail.com>
2022-05-23 14:52:19 +0000

    Modified exp1 parameters

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -33,11 +33,11 @@ def timit_exp1(names,csv_path,noise_dBs, data_cache='/content/AttentionGAN-VC/da
         print(f'Training {name} with SNR {noise_dB}')
 
         run(f'python datasets/fetchData.py --transfer_mode timit --noise_dB {noise_dB}')
-        run(f'python train.py --dataroot data_cache --name {name} --model attention_gan --dataset_mode audio --pool_size 50 --no_dropout --norm instance --lambda_A 10 --lambda_B 10 --lambda_identity 0.5 --load_size_h 128 --load_size_w 128 --crop_size 128 --preprocess resize --batch_size 4 --niter 200 --niter_decay 0 --gpu_ids 0 --display_id 0 --display_freq 100 --print_freq 100 --input_nc 2 --output_nc 2 --use_cycled_discriminators --use_mask --max_mask_len 50 --use_phase --checkpoints_dir /content/drive/MyDrive/TASLP/EXP1/checkpoints')
-        run(f'python test.py --dataroot data_cache --name {name} --model attention_gan --dataset_mode audio --norm instance --phase test --no_dropout --load_size_h 128 --load_size_w 128 --crop_size 128 --batch_size 1 --gpu_ids 0 --input_nc 2 --output_nc 2 --use_mask --use_phase --checkpoints_dir /content/drive/MyDrive/TASLP/EXP1/checkpoints')
+        run(f'python train.py --dataroot data_cache --name {name} --model attention_gan --dataset_mode audio --pool_size 50 --no_dropout --norm instance --lambda_A 10 --lambda_B 10 --lambda_identity 0.5 --load_size_h 128 --load_size_w 128 --crop_size 128 --preprocess resize --batch_size 4 --niter 200 --niter_decay 0 --gpu_ids 0 --display_id 0 --display_freq 100 --print_freq 100 --input_nc 1 --output_nc 1 --use_cycled_discriminators --use_mask --max_mask_len 50 --checkpoints_dir /content/drive/MyDrive/TASLP/EXP1/checkpoints --no_html')
+        run(f'python test.py --dataroot data_cache --name {name} --model attention_gan --dataset_mode audio --norm instance --phase test --no_dropout --load_size_h 128 --load_size_w 128 --crop_size 128 --batch_size 1 --gpu_ids 0 --input_nc 1 --output_nc 1 --use_mask --checkpoints_dir /content/drive/MyDrive/TASLP/EXP1/checkpoints')
         avg_lsd,std_lsd= lsd(os.path.join(data_cache,'noisy','test'),os.path.join(results_dir,name,'test_latest','audios','fake_B'),use_gender=False)
         avg_mssl,std_mssl = mssl(os.path.join(data_cache,'noisy','test'),os.path.join(results_dir,name,'test_latest','audios','fake_B'),use_gender=False)
-        log(csv_path, name,f'Training {name} for EXP1 with SNR {noise_dB} for 200 epochs with cycled_disc, phase and mask. LambdaA & B 10 , lambda_identity 0.5',avg_lsd,std_lsd,avg_mssl,std_mssl)
+        log(csv_path, name,f'Training {name} for EXP1 with SNR {noise_dB} for 200 epochs with cycled_disc, WITHOUT phase and mask. LambdaA & B 10 , lambda_identity 0.5',avg_lsd,std_lsd,avg_mssl,std_mssl)
     
         shutil.rmtree(data_cache)
         print(f'Finished experiment with {name}')
@@ -168,8 +168,8 @@ if __name__ == '__main__':
         df=pd.DataFrame(columns=cols)
         df.to_csv(csv_path,index=False)
     
-    noise_dBs = [0,-3,-6]
-    timit_exp1([f'AttentionGAN_phase_mask_cycdisc_{i}dB' for i in noise_dBs],csv_path,noise_dBs)
+    noise_dBs = [-3,-6]
+    timit_exp1([f'AttentionGAN_nophase_mask_cycdisc_{i}dB' for i in noise_dBs],csv_path,noise_dBs)
     
     #log(csv_path,'Dummy', "Logging default parameters used - 200 lambda_L1, 15% test size",0,0)
 
